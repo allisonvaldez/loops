e61daedcde634cfeb979fa56ebce61648bf76083
Allison Valdez <ajsnipes@usfca.edu>
2022-10-09 21:20:09 -0400

    done

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-/*
-Nested For loop example
-*/
-for (let i = 1; i <= 10; i++) {
-    console.log("OUTERLOOP:", i);
+// Question 1a:
+console.log("1a: Numbers up to ten (using for loop)");
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
 
-    for (let j = 10; j > 0; j--) {
-        console.log("   INNERLOOP:", j);
-    }
+// Question 1b:
+console.log("1b: Numbers up to ten (using while loop)");
+let i = 0;
+while (i <= 10) {
+    console.log(i);
+    i++
+}
+
+// Question 2:
+console.log("2: Even numbers to 20 (using for loop)");
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i);
+}
+
+// Question 3:
+console.log("3: Odd numbers to 20 (using while loop)");
+let j = 1;
+while (j <= 20) {
+    console.log(j);
+    j += 2;
+}
+
+// Question 4:
+console.log("4: Back to 0 (using for loop)");
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
 }
